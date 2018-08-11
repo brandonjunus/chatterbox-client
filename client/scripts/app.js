481b86679423eab02f9a3d846da594e1345c802f
brandonjunus <brandonjunus2@gmail.com>
2018-08-10 21:15:35 -0700

    Working on the chatterbox, just finished roomlist

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,15 +3,33 @@ const app = {};
 app.init = function() {
   $('.username').click(this.handleUsernameClick);
   $('#send .submit').submit(this.handleSubmit);
+  $('#roomSelect').change(this.roomSelect);
+  let params = {
+    order: '-createdAt',
+    limit: 100,
+    where: {
+      // roomname: '4chan',
+    },
+  };
+  app.fetch(params, app.render);
 };
 
-app.server = 'http://parse.sfm6.hackreactor.com/';
+// app.server = 'http://parse.sfm6.hackreactor.com/';
+app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+
+app.render = function(data) {
+  console.log('fetch data', data);
+  for (let message of data.results) {
+    app.renderMessage(message);
+    app.renderRoom(message.roomname);
+  }
+};
 
 app.handleUsernameClick = function() {
   console.log('hello world');
 };
 
-app.handleSubmit = function() {
+app.handleSubmit = function(event) {
   console.log('hello world');
 };
 
@@ -23,7 +41,7 @@ app.send = function(message) {
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log('data', data);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -32,13 +50,15 @@ app.send = function(message) {
   });
 };
 
-app.fetch = function() {
+app.fetch = function(params, cb) {
+  this.clearMessages();
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: this.server,
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages?order=createdat',
     type: 'GET',
-    success: function (data) {
-      console.log('chatterbox: Messages recieved');
+    data: params,
+    success: (data) => {
+      cb(data);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -52,13 +72,39 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  $('#chats').append(
-    `<div>
-      <a href="#" class="username">${message.username}</a><br>
-      ${message.text}
-    </div>`);
+  if (message.text !== undefined) {
+    message.text = message.text.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    $('#chats').append(
+      `<div class='feed'>
+        <a href="#" class="username">${message.username}</a>
+        <br>
+        ${message.text}
+      </div>`);
+  }
 };
 
+var roomNamesObject = {};
+
 app.renderRoom = function(roomName) {
-  $('#roomSelect').append(`<div>${roomName}</div>`);
+  if (!(roomName in roomNamesObject)) {
+    roomNamesObject[roomName] = roomName;
+    $('#roomSelect').append(`<option value=${roomName}>${roomName}</option>`);
+  }
 };
+
+app.roomSelect = function() {
+  app.clearMessages();
+  let params = {
+    order: '-createdAt',
+    limit: 100,
+    where: {
+      roomname: this.value,
+    },
+  };
+  app.fetch(params, app.render);
+};
+
+
+$( document ).ready(function() {
+  app.init();
+});
