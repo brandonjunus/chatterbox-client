431c4fa8f0966b851d2c098609c873d9e20cbe3d
brandonjunus <brandonjunus2@gmail.com>
2018-08-11 15:11:29 -0700

    Complete chatterbox client

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,36 +1,54 @@
 const app = {};
 
+// submit button
+// text box
+// auto-refresh
+
 app.init = function() {
-  $('.username').click(this.handleUsernameClick);
-  $('#send .submit').submit(this.handleSubmit);
-  $('#roomSelect').change(this.roomSelect);
-  let params = {
-    order: '-createdAt',
-    limit: 100,
-    where: {
-      // roomname: '4chan',
-    },
-  };
-  app.fetch(params, app.render);
+  $('#send').click(app.handleSubmit);
+  $('.room-selector').click(app.roomSelect);
+  $('#refresh').click(() => app.fetch(app.params, app.render));
+  $(document).on('click','.username', app.handleUsernameClick);
+  app.fetch(app.params, app.render);
 };
 
 // app.server = 'http://parse.sfm6.hackreactor.com/';
 app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
 
 app.render = function(data) {
-  console.log('fetch data', data);
   for (let message of data.results) {
     app.renderMessage(message);
     app.renderRoom(message.roomname);
   }
 };
 
-app.handleUsernameClick = function() {
-  console.log('hello world');
+app.friends = {};
+
+app.handleUsernameClick = function(event) {
+  var username = event.target.innerHTML;
+  if (!(username in app.friends)){
+    app.friends[username] = username;
+  // <li class="list-group-item">Cras justo odio</li>
+    $("#friends-list").append(`<h6 class="list-group-item text-left border-0">${username}</h6>`)
+    console.log("friendslist", app.friends)
+  }
+  // console.log('hello world');
 };
 
 app.handleSubmit = function(event) {
-  console.log('hello world');
+  let message = {
+    username: app.username,
+    text: $('#message-text').val(),
+    roomname: $('#roomname-text').val(),
+  };
+  app.send(message);
+  app.fetch(app.params, app.render);
+};
+
+app.params = {
+  order: '-createdAt',
+  limit: 100,
+  // where: { roomname: '4chan' },
 };
 
 app.send = function(message) {
@@ -51,13 +69,15 @@ app.send = function(message) {
 };
 
 app.fetch = function(params, cb) {
-  this.clearMessages();
+  app.clearMessages();
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages?order=createdat',
+    url: this.server,
     type: 'GET',
     data: params,
     success: (data) => {
+      console.log(app.params)
+      console.log(data);
       cb(data);
     },
     error: function (data) {
@@ -74,12 +94,16 @@ app.clearMessages = function() {
 app.renderMessage = function(message) {
   if (message.text !== undefined) {
     message.text = message.text.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    $('#chats').append(
-      `<div class='feed'>
-        <a href="#" class="username">${message.username}</a>
-        <br>
-        ${message.text}
-      </div>`);
+    $('#chats').hide().append(
+      `<a href="#" class="list-group-item list-group-item-action flex-column align-items-start px-5">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1 username">${message.username}</h5>
+          <small>${moment(message.createdAt).fromNow()}</small>
+        </div>
+        <p class="mb-1">${message.text}</p>
+        <small>${message.roomname}</small>
+      </a>`
+    ).fadeIn();
   }
 };
 
@@ -88,23 +112,18 @@ var roomNamesObject = {};
 app.renderRoom = function(roomName) {
   if (!(roomName in roomNamesObject)) {
     roomNamesObject[roomName] = roomName;
-    $('#roomSelect').append(`<option value=${roomName}>${roomName}</option>`);
+    $('#roomSelect').append(`<a class="dropdown-item room-selector" href="#" onclick='app.roomSelect("${roomName}")'>${roomName}</a>`);
   }
 };
 
-app.roomSelect = function() {
-  app.clearMessages();
-  let params = {
-    order: '-createdAt',
-    limit: 100,
-    where: {
-      roomname: this.value,
-    },
-  };
-  app.fetch(params, app.render);
+app.roomSelect = function(roomName) {
+  app.params.where = { roomname: roomName };
+  app.fetch(app.params, app.render);
 };
 
+app.username = '';
 
 $( document ).ready(function() {
+  app.username = window.location.search.replace('?username=', '').replace('%20', ' ');
   app.init();
 });
